faf788f82b94e51871c9eb81b7f85d6060628188
Anand Chembarpu <anandchembarpu@gmail.com>
2019-11-23 18:12:06 +0530

    User connection and disconnection setup

--- client/src/components/Lobby/Lobby.js
+++ client/src/components/Lobby/Lobby.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./Lobby.css";
+
 const Lobby = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
